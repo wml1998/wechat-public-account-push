ce8bdfac1d9a019486a34feffaabca75852e1968
wml1998 <49548342+wml1998@users.noreply.github.com>
2023-06-07 23:35:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,22 +29,22 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '小鼠倩', year: '1999', date: '02-09',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '小鼠倩', year: '1999', date: '03-26',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '开始日', year: '2023', date: '02-14',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-02-14' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+//         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],

8218b4553c6c659c6f646f67692cb9fdcda79026
wml1998 <49548342+wml1998@users.noreply.github.com>
2023-07-29 22:32:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-02-14' },
+        { keyword: 'love_day', date: '2023-02-04' },
         // 结婚纪念日
 //         { keyword: 'marry_day', date: '2022-09-09' },
       ],

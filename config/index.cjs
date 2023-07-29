6bc51e6e55b54815fe53ed884e81cb10a70a8b3a
wml1998 <49548342+wml1998@users.noreply.github.com>
2023-07-29 16:30:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小鼠倩',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'Ub3b_QlXUGjn5DINqcghEULdIIf1lEp7Iy80nt8k6JE',
+      id: 'owH106k3YnIkbLIdLaRth_zjeZZ4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Ub3b_QlXUGjn5DINqcghEULdIIf1lEp7Iy80nt8k6JE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD

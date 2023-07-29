1f5c68cc7bcca2864ebed6a45746280e63d31e81
wml1998 <49548342+wml1998@users.noreply.github.com>
2023-07-29 18:57:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'owH106k3YnIkbLIdLaRth_zjeZZ4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'HEwvu-mlGjrPPLW8rkvT1nGytBZrftlzHlvSubJUuAc',
+      useTemplateId: 'A52gs8FMC9eWxxtFkt2GlZ7ry-gLifzuTHqsFKa_MyM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [

fb394a632083e394a9c0c3bcb12290d35d0b62a5
wml1998 <49548342+wml1998@users.noreply.github.com>
2023-07-29 16:58:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'owH106k3YnIkbLIdLaRth_zjeZZ4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Ub3b_QlXUGjn5DINqcghEULdIIf1lEp7Iy80nt8k6JE',
+      useTemplateId: 'sUA0XcgKey5btWeuKwdlQu_upTj6YhoJvTATq9FFwzw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'sUA0XcgKey5btWeuKwdlQu_upTj6YhoJvTATq9FFwzw',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'owH106k3YnIkbLIdLaRth_zjeZZ4',
     }
   ],
 
